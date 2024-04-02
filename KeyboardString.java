11f925cdd4c8f25956ced5749bee1a140736e0b4
anirudhreddy9966 <139291438+anirudhreddy9966@users.noreply.github.com>
2024-04-02 01:59:07 -0500

    Add files via upload

---
+++ KeyboardString.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+
+public class KeyboardString
+{
+   public static void main(String args[])
+   {
+    
+     Scanner sc=new Scanner(System.in);
+     String name;
+     System.out.println("May is know your name");
+     name=sc.nextLine();
+     System.out.println("My name is " + name);
+   }
+}
